70af2ca7b9120cb691232ff3142ded596fb0db54
Mariam Haider <mfh.haider@gmail.com>
2020-04-27 17:48:33 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,32 +1,5 @@
-class Show < ActiveRecord::Base
-  
-  def highest_rating
-    Show.maximum(:rating)
-  end 
-  
-  def most_popular_show
-    Show.where(rating: highest_rating).first 
-  end 
-  
-  def lowest_rating
-    Show.minimum(:rating)
-  end 
-  
-  def least_popular_show
-    Show.where(rating: lowest_rating).first 
-  end 
-  
-  def ratings_sum 
-    Show.sum(:rating)
-  end 
-  
-  def popular_shows 
-    Show.where("rating > '5'" )
-  end 
-  
-  def shows_by_alphabetical_order
-    Show.order(name: :asc)
-  end 
-  
-  
-end 
+class AddSeasonToShows < ActiveRecord::Migration[5.2]
+    def change 
+        add_column :shows, :season, :string
+    end
+end
