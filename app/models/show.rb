3e9653ad345b45a246ef481c1bfbbc928dfed8c0
Mariam Haider <mfh.haider@gmail.com>
2020-04-27 17:44:58 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -5,6 +5,16 @@ class Show < ActiveRecord::Base
   end 
   
   def most_popular_show
-    Show.where(rating = highest_rating).
+    Show.where(rating: highest_rating).first 
   end 
+  
+  def lowest_rating
+    Show.minimum(:rating)
+  end 
+  
+  def least_popular_show
+    Show.where(rating: lowest_rating).last 
+  end 
+  
+  
 end 
